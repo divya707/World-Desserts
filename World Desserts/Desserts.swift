a9a33643adbd681fa42169ab9f5c022b916d21ed
Sanaye Lewis <sanayetalsk@gmail.com>
2020-06-11 14:48:44 -0400

    Commit 10

---
+++ World Desserts/Desserts.swift
@@ -0,0 +1,14 @@
+//
+//  Desserts.swift
+//  World Desserts
+//
+//  Created by Sanaye Lewis on 6/11/20.
+//  Copyright © 2020 Divya. All rights reserved.
+//
+
+import UIKit
+
+class Desserts {
+    
+    var name = ""
+}
